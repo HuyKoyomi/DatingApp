ade876e376ecc6e98bf1540b73658507496eafa0
DESKTOP-FJA9VLT\Dell <huylm6@fpt.com>
2025-01-07 15:10:24 +0700

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 
 // Định nghĩa decorator @Component
 @Component({
   selector: 'app-root', // Định nghĩa tên thẻ HTML mà component này sẽ gắn vào
   standalone: true, // Chỉ định rằng đây là một thành phần độc lập (standalone component), không cần khai báo trong một module
-  imports: [RouterOutlet], // Khai báo các dependencies mà component này cần => component sử dụng RouterOutlet để xử lý điều hướng.
+  imports: [NavComponent, HomeComponent], // Khai báo các dependencies mà component này cần => component sử dụng RouterOutlet để xử lý điều hướng.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -14,15 +16,17 @@ export class AppComponent implements OnInit {
   http = inject(HttpClient);
   title = 'DatingApp';
   users: any;
+  private accountService = inject(AccountService);
 
+  // ham khoi tao khi chay vao component
   ngOnInit(): void {
-    // throw new Error('Method not implemented.');
-    this.http.get('https://localhost:5000/api/users').subscribe({
-      next: (response) => (this.users = response),
-      error: (error) => console.error(error),
-      complete: () => console.log('Request has completed'),
-    });
+    this.setCurrentUser();
   }
 
-  // constructor(private httpClient: HttpClient) {}
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
 }
