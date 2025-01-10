81ea8a6db82e98a9b68178d61b9576505f5dc5a9
DESKTOP-FJA9VLT\Dell <huylm6@fpt.com>
2025-01-10 15:43:48 +0700

    End of section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { AccountService } from './_services/account.service';
-import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
+import { RouterOutlet } from '@angular/router';
 
 // Định nghĩa decorator @Component
 @Component({
   selector: 'app-root', // Định nghĩa tên thẻ HTML mà component này sẽ gắn vào
   standalone: true, // Chỉ định rằng đây là một thành phần độc lập (standalone component), không cần khai báo trong một module
-  imports: [NavComponent, HomeComponent], // Khai báo các dependencies mà component này cần => component sử dụng RouterOutlet để xử lý điều hướng.
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [NavComponent, RouterOutlet], // Khai báo các dependencies mà component này cần => component sử dụng RouterOutlet để xử lý điều hướng.
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,29 +2,40 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { AccountService } from '../_services/account.service';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [
+    FormsModule,
+    BsDropdownModule,
+    RouterLink,
+    RouterLinkActive,
+    TitleCasePipe,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
 export class NavComponent {
   accountService = inject(AccountService);
-
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: (response) => {
-        console.log((this.model.userName = response));
+      next: () => {
+        this.router.navigateByUrl('/members');
       },
-      error: (error) => console.error(error),
+      error: (error) => this.toastr.error(error.error),
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
