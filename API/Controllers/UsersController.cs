dda2b8321279291c286b82c8e339cba26145743a
DESKTOP-FJA9VLT\Dell <huylm6@fpt.com>
2025-01-21 11:14:59 +0700

    113. BE - Update User API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 
 
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -12,22 +13,45 @@ namespace API.Controllers;
 
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController : BaseApiController
 {
 
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
+
+    public UsersController(IUserRepository userRepository, IMapper mapper)
+    {
+        _userRepository = userRepository;
+        _mapper = mapper;
+    }
     // [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await userRepository.GetMembersAsync();
+        var users = await _userRepository.GetMembersAsync();
         return Ok(users);
     }
 
     [HttpGet("{username}")]
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await userRepository.GetMemberAsync(username);
+        var user = await _userRepository.GetMemberAsync(username);
         if (user == null) return NotFound();
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; // lấy username từ claims của người dùng hiện tại
+        if (username == null) return BadRequest("No username found in token!");
+
+        var user = await _userRepository.GetUserByUsernameAsync(username);
+        if (user == null) return BadRequest("Counld not find user!");
+
+        _mapper.Map(memberUpdateDto, user);
+        if (await _userRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update the user");
     }
 }
