1a1f92ce7c8e51cd72a48ece2cc22a79a4e2e6fd
DESKTOP-FJA9VLT\Dell <huylm6@fpt.com>
2025-02-05 15:23:11 +0700

    172 - FE - Adding the toggle functionality

--- client/src/app/members/member-card/member-card.component.ts
+++ client/src/app/members/member-card/member-card.component.ts
@@ -1,6 +1,13 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  ViewEncapsulation,
+} from '@angular/core';
 import { Member } from '../../_models/member';
 import { RouterLink } from '@angular/router';
+import { LikesService } from '../../_services/likes.service';
 
 @Component({
   selector: 'app-member-card',
@@ -11,5 +18,23 @@ import { RouterLink } from '@angular/router';
   encapsulation: ViewEncapsulation.None, // CSS của component này sẽ áp dụng toàn cục lên toàn bộ ứng dụng (giống như CSS thông thường). -Không bị đóng gói vào riêng component.
 })
 export class MemberCardComponent {
+  private likeSvc = inject(LikesService);
   member = input.required<Member>();
+  hasLiked = computed(() => this.likeSvc.likeIds().includes(this.member().id));
+
+  toggleLike() {
+    this.likeSvc.toggleLike(this.member().id).subscribe({
+      next: () => {
+        if (this.hasLiked()) {
+          // bỏ thích
+          this.likeSvc.likeIds.update((ids) =>
+            ids.filter((x) => x !== this.member().id)
+          );
+        } else {
+          // yêu thích
+          this.likeSvc.likeIds.update((ids) => [...ids, this.member().id]);
+        }
+      },
+    });
+  }
 }
